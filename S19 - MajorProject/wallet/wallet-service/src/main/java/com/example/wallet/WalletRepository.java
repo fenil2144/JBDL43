a0d5bcd25883ee6c089aa987bac878a981962f09
fenil2144 <shahfenil1998@gmail.com>
2023-03-12 21:11:19 +0530

    Session 20

--- S19 - MajorProject/wallet/wallet-service/src/main/java/com/example/wallet/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.wallet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Integer>{
-	Wallet findByPhoneNumber(String phoneNumber);
-}
